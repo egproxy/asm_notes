187007184d8f2da638edda422afa7e5a41db29b9
egproxy <eg.proxy@gmail.com>
2015-05-23 17:45:40 -0700

    switch in progress

--- FileHandling/readFile.s
+++ FileHandling/readFile.s
@@ -10,10 +10,13 @@
 # File Descriptor Modes are found in /usr/include/asm/fcntl.h
 # Defined constants are in octal format
   .equ MODE_RO, 0x0
+  .equ MODE_RW, 0x1
+  .equ MODE_CREAT, 0x40     # octal 100
+  .equ MODE_TRUNC, 0x200    # octal 1000
 
   .section .data
 
-LC0: .asciz "Got File Descriptor: %i\n"
+LC0: .asciz "Invalid File Descriptor: %i\n"
 
   .section .text
 
@@ -22,35 +25,64 @@ LC0: .asciz "Got File Descriptor: %i\n"
 main:
   pushl       %ebp
   movl        %esp,         %ebp
-  subl        $8,           %esp
+  subl        $16,          %esp
+
   movl        12(%ebp),     %eax
-  
   movl        4(%eax),      %eax 
   movl        $MODE_RO,     4(%esp)
   movl        %eax,         (%esp)
-  call        readfile
-
-  movl        %eax,         4(%esp)
-  movl        $LC0,         (%esp)
-  call        printf
+  call        openfile
   
+  cmpl        $0,           %eax 
+  jl          invalid_fd
+
+ ## BOTTOM OF STACK HOLDS READ FILE
+  movl        %eax,         12(%esp)
+  
+  movl        14(%ebp),     %eax
+  movl        4(%eax),      %eax
+  movl        $MODE_RW,     4(%esp)
+  orl         $MODE_CREAT,  4(%esp)
+  orl         $MODE_TRUNC,  4(%esp)
+  movl        %eax,         (%esp)
+  call        openfile
+
+  cmpl        $0,           %eax 
+  jl          invalid_fd 
+
+ ## 2ND TO BOTTOM HOLDS OUTPUT FILE 
+  movl        %eax,         8(%esp)
+
+ ## CLOSE OPEN FILES 
+  movl        12(%esp),     %eax
   movl        %eax,         (%esp)
   call        closefile
 
+  movl        8(%esp),      %eax
+  movl        %eax,         (%esp)
+  call        closefile
+
+  jmp         fin
+invalid_fd:
+  movl        %eax,         4(%esp)
+  movl        $LC0,         (%esp)
+  call        printf
+
+fin: 
   addl        $8,           %esp        # dealloc stack space
   movl        $1,           %eax
   movl        $0,           %ebx
   int         $0x80
   
-# Function: readfile
+# Function: openfile
 #
 # Parameters: 
 # 8(%ebp)  ::  address of cstring
 # 12(%ebp) ::  file mode
 #
 # Return file descriptor in %eax
-.type readfile, @function
-readfile:
+.type openfile, @function
+openfile:
   pushl       %ebp
   movl        %esp,         %ebp
   movl        $FOPEN,       %eax
